467d11b51b4f4882e86ed8a71380a2a464c80297
ChanceMaston <chance.maston@cradlepoint.com>
2021-04-08 10:42:50 -0600

    Added ability to route to differnt lists in ToDo App

--- Fyyr/ToDo_App/app.py
+++ Fyyr/ToDo_App/app.py
@@ -76,9 +76,14 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+                           todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
 
 if __name__ == '__main__':
     app.run()
